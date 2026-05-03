ec16246242e9d300dbf0e9c8a830205142496431
Hunter Parcells <hunterparcells@gmail.com>
2026-05-03 00:13:50 -0400

    feat: Update docs url.

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
     v4: true
   },
 
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://picdocs.hunterparcells.com',
   baseUrl: '/',
 
   organizationName: 'hparcells',
